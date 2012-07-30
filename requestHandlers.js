f4ee29539cdc7615e63c4a384a9c565eebba1fe7
kalin <kalin@10-196-2-211.wireless.oregonstate.edu>
2012-07-30 10:37:31 -0700

    moving files and setting up new node server

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,13 +1,47 @@
 var exec = require("child_process").exec;
 
+var fs = require("fs");
+
 function start(response) {
 	console.log("Request handler 'start' was called.");
-
-	var body = "<p>what what</p>";
-	response.writeHead(200, {"Content-Type": "text/html"});        
-	response.write(body);
-	response.end();
+    
+    var filePath = '.' + request.url;
+    if (filePath == './')
+        filePath = './index.htm';
+    
+    var extname = path.extname(filePath);
+    var contentType = 'text/html';
+    switch (extname) {
+        case '.js':
+            contentType = 'text/javascript';
+            break;
+        case '.css':
+            contentType = 'text/css';
+            break;
+    }
+    
+    path.exists(filePath, function(exists) {
+                
+                if (exists) {
+                fs.readFile(filePath, function(error, content) {
+                            if (error) {
+                            response.writeHead(500);
+                            response.end();
+                            }
+                            else {
+                            response.writeHead(200, { 'Content-Type': contentType });
+                            response.end(content, 'utf-8');
+                            }
+                            });
+                }
+                else {
+                response.writeHead(404);
+                response.end();
+                }
+                });
 }
+
+            
 
 function upload(response) {
     console.log("Request handler 'upload' was called.");
--- server.js
+++ server.js
@@ -1,18 +1,50 @@
-var http = require("http");
-var url = require("url");
+//var http = require("http");
+//var url = require("url");
+//
+//var static = require("node-static");
+//
+//function start(route, handle) {
+//  function onRequest(request, response) {
+//    var pathname = url.parse(request.url).pathname;
+//      
+//    console.log("Request for " + pathname + " received.");
+//
+//    route(handle, pathname, response);
+//
+//  }
+//
+//  http.createServer(onRequest).listen(8888);
+//  console.log("Server has started.");
+//}
+//
+//exports.start = start;
+//
 
-function start(route, handle) {
-  function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    console.log("Request for " + pathname + " received.");
+var static = require('node-static'),
+http = require('http'),
+util = require('util');
 
-    route(handle, pathname, response);
-
-  }
-
-  http.createServer(onRequest).listen(8888);
-  console.log("Server has started.");
-}
-
-exports.start = start;
-
+var webroot = './public',
+port = 8888;
+var file = new(static.Server)(webroot, {
+                              cache: 600,
+                              headers: { 'X-Powered-By': 'node-static' }
+                              });
+http.createServer(function(req, res) {
+                  req.addListener('end', function() {
+                                  file.serve(req, res, function(err, result) {
+                                             if (err) {
+                                             console.error('Error serving %s - %s', req.url, err.message);
+                                             if (err.status === 404 || err.status === 500) {
+                                             file.serveFile(util.format('/%d.html', err.status), err.status, {}, req, res);
+                                             } else {
+                                             res.writeHead(err.status, err.headers);
+                                             res.end();
+                                             }
+                                             } else {
+                                             console.log('%s - %s', req.url, res.message);
+                                             }
+                                             });
+                                  });
+                  }).listen(port);
+console.log('node-static running at http://localhost:%d', port);
